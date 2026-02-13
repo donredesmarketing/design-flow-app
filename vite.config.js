61825f19f8f3613f565b550b7711493dc3447989
donredesmarketing <donredesmarketing@gmail.com>
2026-02-13 17:24:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante para Hostinger
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      },
+  base: './',
+  resolve: {
+    alias: {
+      '@': '/src',
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 })
