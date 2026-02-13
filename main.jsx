ed131494caf8b7bf7d28d1ae017d75df27470e2a
donredesmarketing <donredesmarketing@gmail.com>
2026-02-13 17:25:18 +0100

    Remove unused CSS import from main.jsx
    Removed import statement for global styles.

--- main.jsx
+++ main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Si tienes estilos globales, si no, puedes borrar esta línea
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
